7c8a8c7a5d9ea9a3ddf2e1127e345e2c2336165b
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2017-10-14 19:33:46 -0400

    .

--- RenamingDiskSol/RenamingDisk/CreateSnapshots.ps1
+++
@@ -1,9 +0,0 @@
-$rgName = ‘ren’ # or the Resource Group name you used
-Get-AzureRmDisk -ResourceGroupName $rgName | foreach `
-{
-    $disk = $_
-    Write-Host "Disk:  ", $disk.Name
-
-    $config = New-AzureRmSnapshotConfig -SourceUri $disk.Id -CreateOption Copy -Location $disk.Location -SkuName PremiumLRS
-    $snapshot = New-AzureRmSnapshot -Snapshot $config -SnapshotName ($disk.Name+’-snapshot’) -ResourceGroupName $rgName
-}
